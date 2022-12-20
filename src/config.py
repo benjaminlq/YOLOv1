64c36fc0cd9980e357a068a067b572ec9eb64c96
benjaminlq <benjamin.lequan@gmail.com>
2022-12-20 22:08:08 +0800

    First Commit

---
+++ src/config.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+MAIN_PATH = Path(__file__).resolve(0).parents[1]
+DATA_PATH = MAIN_PATH / "data"
+ARTIFACT_PATH = MAIN_PATH / "artifacts"
---
+++ src/utils.py
@@ -0,0 +1,21 @@
+import torch
+
+def intersection_over_union(
+    self,
+    boxes_preds: list,
+    boxes_labels: list,
+    
+):
+    return iou
+    
+def mean_average_precision(
+    self,
+    
+):
+    return mAP
+    
+def non_max_suppression(
+    self,
+):
+    return nmp
+
